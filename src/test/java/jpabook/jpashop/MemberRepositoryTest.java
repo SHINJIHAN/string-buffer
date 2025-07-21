83860ff1330afe231c7099f6336601a92d449031
sinjihan71 <sinjihan71@gmail.com>
2025-07-21 21:08:57 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        Member member = new Member();
+        member.setName("memberA");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member); // JPA 동일성 보장
+        System.out.println("findMember == member: " + (findMember == member));
+    }
+}
