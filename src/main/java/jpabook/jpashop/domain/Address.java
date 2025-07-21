85fb5f79dfb856c1490dd47518cd7f29c701e5a2
sinjihan71 <sinjihan71@gmail.com>
2025-07-21 22:30:12 +0900

    Address.java

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Embeddable;
 import lombok.Getter;
+import jakarta.persistence.Embeddable;
 
 @Embeddable
 @Getter
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+    protected Address() {
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
