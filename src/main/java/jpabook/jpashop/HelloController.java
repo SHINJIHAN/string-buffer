4b75e91ec276859dfe0ba6e774117546e36b7a3a
SHINJIHAN <135806819+SHINJIHAN@users.noreply.github.com>
2025-08-13 19:58:36 +0900

    Update HelloController.java

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
